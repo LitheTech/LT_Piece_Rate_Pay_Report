9d7e794c2832ad08df2df49e332e559b60279441
RafeedMohammad <rasheedtahmeed@gmail.com>
2026-01-18 14:18:53 +0600

    modified pr status report

--- lt_piece_rate_pay_report/lt_piece_rate_pay_report/report/production_status_report/production_status_report.py
+++ lt_piece_rate_pay_report/lt_piece_rate_pay_report/report/production_status_report/production_status_report.py
@@ -22,10 +22,10 @@ def get_columns():
         _("Process Type") + ":Data:200",
         # _("Style") + ":Data:90",
         # _("Sales Contract") + ":Data:200",
-        _("Bill Qty (pcs)") + ":Int:100",
-        _("Bill Qty(Dzn)") + ":Int:100",
-		_("Pcs Rate") + ":Int:100",
-		# _("Bill") + ":Int:100",
+        _("Total Qty") + ":Int:100",
+        _("Completed Qty") + ":Int:100",
+		_("Bill Qty") + ":Int:100",
+		_("Remain Qty") + ":Int:100",
 
         # _("Stamp Deduct") + ":Int:100",
 		# _("Total Bill") + ":Int:100",
@@ -42,9 +42,10 @@ def get_data(filters):
             dp.production_date,
             dp.color,
             dp.process_type,
-            dp.	total_quantity,
+            dp.total_quantity,
             dp.completed_quantity,
-            dp.bill_quantity
+            dp.bill_quantity,
+			dp.total_quantity-dp.completed_quantity-dp.bill_quantity
             
         FROM `tabDaily Production` dp
         JOIN `tabDaily Production Details` dpd ON dp.name = dpd.parent
